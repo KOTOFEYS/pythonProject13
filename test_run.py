28b7935597e024b101d5ff3c83a46e3571589b99
KOTOFEYS <makarov.k@mail.ru>
2024-11-05 22:14:45 +0300

    Домашнее задание по теме "Методы Юнит-тестирования"

--- test_run.py
+++ test_run.py
@@ -1,23 +1,30 @@
-import runner
+import runner1
 import unittest
 
 class RunnerTest(unittest.TestCase):
 
+    is_frozen = False
+
+    @unittest.skipIf(is_frozen ,'Тесты в этом кейсе заморожены')
     def test_walk(self):
-        run_1 = runner.Runner('Urmas')
+        run_1 = runner1.Runner('Urmas')
         while run_1.walk() in range(10):
             self.assertEqual(run_1.distance, 50)
 
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_run(self):
-        run_2 = runner.Runner('Iihu')
+        run_2 = runner1.Runner('Iihu')
         while run_2.walk() in range(10):
             self.assertEqual(run_2.distance, 100)
 
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_challenge(self):
-        run_1 = runner.Runner('Urmas')
-        run_2 = runner.Runner('Iihu')
+        run_1 = runner1.Runner('Urmas')
+        run_2 = runner1.Runner('Iihu')
         while run_1.walk() and run_2.run() in range(10):
+
             self.assertNotEqual(run_1.distance, run_2.distance)
+
 
 if __name__ == '__main__':
     unittest.main()
