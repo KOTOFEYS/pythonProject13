a3f8351f26af81c6f9c006b8d6fb4b55f9574085
KOTOFEYS <makarov.k@mail.ru>
2024-11-05 22:15:24 +0300

    Домашнее задание по теме "Систематизация и пропуск тестов".

--- test_runner1.py
+++ test_runner1.py
@@ -3,28 +3,32 @@ import runner1
 import unittest
 
 class TournamentTest(unittest.TestCase):
-
+    is_frozen = True
     @classmethod
     def setUpClass(cls):
         cls.all_results = {}
 
+    @unittest.skipIf(is_frozen ,'Тесты в этом кейсе заморожены')
     def setUp(self):
         self.runner_1 = runner1.Runner('Усейн', 10)
         self.runner_2 = runner1.Runner('Андрей', 9)
         self.runner_3 = runner1.Runner('Ник', 3)
 
-
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_race3(self):
         race = runner1.Tournament(90,  self.runner_1, self.runner_2, self.runner_3)
         self.all_results = race.start()
         TournamentTest.all_results.update(self.all_results)
         self.assertTrue(self.all_results[len(self.all_results)], 'Ник')
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_race1(self):
         race = runner1.Tournament(90,  self.runner_1, self.runner_3)
         self.all_results = race.start()
         TournamentTest.all_results.update(self.all_results)
         self.assertTrue(self.all_results[len(self.all_results)], 'Ник')
 
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_race2(self):
         race = runner1.Tournament(90,  self.runner_2, self.runner_3)
         self.all_results = race.start()
